eb636791620d51d1b22462d0f3a1a50cf225f9aa
Brandon Johnson <brandon@moov.io>
2022-06-27 01:07:25 -0600

    binary search, insertion sort, and selection sort algorithms in java

---
+++ algorithms/java/BinarySearch.java
@@ -0,0 +1,19 @@
+package main;
+
+public class BinarySearch {
+  public static int search(int[] array, int target) {
+    int low = 0;
+    int high = array.length - 1;
+    while (low <= high) {
+      int mid = (low + high) / 2;
+      if (array[mid] == target) {
+        return mid;
+      } else if (array[mid] < target) {
+        low = mid + 1;
+      } else {
+        high = mid - 1;
+      }
+    }
+    return -1;
+  }
+}
