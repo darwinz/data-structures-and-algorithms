ed4b8d21cb61c3a0a135018da9d789cb3907372c
Brandon Johnson <brandon@moov.io>
2022-07-06 03:03:02 -0600

    fixed up mergesort for golang

--- algorithms/go/mergesort.go
+++ algorithms/go/mergesort.go
@@ -7,5 +7,27 @@ func mergesort(arr []int) []int {
 	mid := len(arr) / 2
 	left := mergesort(arr[:mid])
 	right := mergesort(arr[mid:])
-	return mergesort(left, right)
+	return merge(left, right)
+}
+
+func merge(a []int, b []int) []int {
+	final := []int{}
+	i := 0
+	j := 0
+	for i < len(a) && j < len(b) {
+		if a[i] < b[j] {
+			final = append(final, a[i])
+			i++
+		} else {
+			final = append(final, b[j])
+			j++
+		}
+	}
+	for ; i < len(a); i++ {
+		final = append(final, a[i])
+	}
+	for ; j < len(b); j++ {
+		final = append(final, b[j])
+	}
+	return final
 }
