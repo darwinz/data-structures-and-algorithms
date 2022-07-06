5b78625f8afe3e5245189f9c667e78062d989b43
Brandon Johnson <brandon@moov.io>
2022-07-06 03:17:17 -0600

    fixed up quicksort in golang

--- algorithms/go/quicksort.go
+++ algorithms/go/quicksort.go
@@ -1,17 +1,37 @@
 package main
 
+import "crypto/rand"
+
+func generateSlice(size int) []int {
+	arr := make([]int, size)
+	for i := 0; i < size; i++ {
+		arr[i] = i
+	}
+	return arr
+}
+
 func quicksort(arr []int) []int {
 	if len(arr) < 2 {
 		return arr
 	}
-	pivot := arr[0]
-	left, right := []int{}, []int{}
-	for _, v := range arr[1:] {
-		if v < pivot {
-			left = append(left, v)
-		} else {
-			right = append(right, v)
+
+	left, right := 0, len(a)-1
+
+	pivot := rand.Int() % len(a)
+
+	arr[pivot], arr[right] = arr[right], arr[pivot]
+
+	for a, v := range arr {
+		if v < arr[right] {
+			arr[left], arr[a] = arr[a], arr[left]
+			left++
 		}
 	}
-	return append(append(quicksort(left), pivot), quicksort(right)...)
+
+	arr[left], arr[right] = arr[right], arr[left]
+
+	quicksort(arr[:left])
+	quicksort(arr[left+1:])
+
+	return arr
 }
